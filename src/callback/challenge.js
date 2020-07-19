560f5096ba4e222023c1bbf6e117cda05a22a23f
jsr25 <ramosjuans@live.com.ar>
2020-07-19 13:13:43 -0500

    Se realizo la peticion con callbacks

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,4 +1,5 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API ='https://rickandmortyapi.com/api/character/'
 
 function fetchData(urlApi,callback){
     let xhttp = new XMLHttpRequest();
@@ -14,6 +15,17 @@ function fetchData(urlApi,callback){
       }  
     }
     xhttp.send();
-
-
 }
+
+fetchData(API,function(error1,data1){
+  if(error1) return console.error(error1);
+  fetchData(API + data1.results[0].id, function (error2, data2){
+    if(error2) return console.error(erro2);
+    fetchData(data2.origin.url, function (error3, data3){
+      if(error3) return console.error(error3);
+      console.log(data1.results.length);
+      console.log(data2.name);
+      console.log(data3.dimension);
+    })
+  })
+});
