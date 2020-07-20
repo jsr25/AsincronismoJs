f602ee3829d7abaa59036ce5a54e1c8ed115b922
jsr25 <ramosjuans@live.com.ar>
2020-07-20 00:42:41 -0500

    Challenge async-await

---
+++ src/async/challenge.js
@@ -0,0 +1,21 @@
+const fetchData = require("../utils/fetchData");
+
+const API = 'https://rickandmortyapi.com/api/character/';
+
+const anotherFunction = async (urlApi) => {
+    try{
+        const data = await fetchData(urlApi);
+        const character = await fetchData(`${API}${data.results[0].id}`)
+        const origin = await fetchData(character.origin.url);
+
+        console.log(data.info.count)
+        console.log(character.name)
+        console.log(origin.dimension)
+    }catch(err){
+        console.error(err)
+    }
+}
+
+console.log("Befort")
+anotherFunction(API);
+console.log('After')
